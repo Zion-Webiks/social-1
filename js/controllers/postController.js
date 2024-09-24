bd9e5984e7238477cf7cd2a89ef6ed3bed7cfa0b
Yosi <Yosi@YosiWebiks.com>
2024-09-24 17:53:49 +0300

    all EP template - done

--- js/controllers/postController.js
+++ js/controllers/postController.js
@@ -1,3 +1,84 @@
 import exp from 'express';
 const router = exp.Router();
+router.get('/', async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: 'I was way too lazy to change the default message',
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: 'I was way too lazy to change the default message',
+            data: null
+        });
+    }
+});
+router.post('/', async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: 'I was way too lazy to change the default message',
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: 'I was way too lazy to change the default message',
+            data: null
+        });
+    }
+});
+// query params: ?title=x&date=23/04/2015
+router.get('/search', async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: 'I was way too lazy to change the default message',
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: 'I was way too lazy to change the default message',
+            data: null
+        });
+    }
+});
+router.get('/:id', async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: 'I was way too lazy to change the default message',
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: 'I was way too lazy to change the default message',
+            data: null
+        });
+    }
+});
+router.patch('/like/:id', async (req, res) => {
+    try {
+        res.status(200).json({
+            err: false,
+            message: 'I was way too lazy to change the default message',
+            data: undefined
+        });
+    }
+    catch (err) {
+        res.status(400).json({
+            err: true,
+            message: 'I was way too lazy to change the default message',
+            data: null
+        });
+    }
+});
 export default router;
