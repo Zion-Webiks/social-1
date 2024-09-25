69df5fd52661a592ba7083578a173bc74dcdb450
Yosi <Yosi@YosiWebiks.com>
2024-09-25 11:21:23 +0300

    user+post models done

---
+++ src/models/user.ts
@@ -0,0 +1,20 @@
+import { v4 } from "uuid"
+
+class User {
+     public id: string
+     public followeres: string[] = []
+     public following:  string[] = []
+     public isLockedAccount:boolean = false
+     public token?:string
+    constructor(
+       public username:string,
+       public password:string,
+       public email:   string,
+       public birthday:Date,
+       public avatarUrl:string,
+    ) {
+        this.id = v4()
+     }
+}
+
+export default User
